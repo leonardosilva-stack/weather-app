a714f833955e991d58d25804f2d6377340cb35b2
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-04-21 01:01:34 -0300

    feat: create interface weatherDatas

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'Santos';
+  weatherDatas!: WeatherDatas;
 
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName)
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) =>{
+        response && (this.weatherDatas = response);
+
+      },
+      error: (error) => console.log(error),
+    })
+  }
 }
