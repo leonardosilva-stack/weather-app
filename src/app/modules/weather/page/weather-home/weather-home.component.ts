c43d91f5a6e88e2d0fc0f97f063742bd3307f870
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-04-21 15:16:34 -0300

    feat: create weather-card

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas.interface';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+
   initialCityName = 'Santos';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService){}
 
@@ -19,12 +24,25 @@ export class WeatherHomeComponent implements OnInit {
 
   getWeatherDatas(cityName: string): void {
     this.weatherService.getWeatherDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
     .subscribe({
       next: (response) =>{
         response && (this.weatherDatas = response);
-
+        console.log(response)
       },
       error: (error) => console.log(error),
     })
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-  private apiKey = '6869e5209518c04f0cccff247765620c';
+  private apiKey = '6382bcc26479515eedc5eecb7474992f';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getWeatherDatas(cityName: string): Observable<any>{
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&apiid=${this.apiKey}`, {})
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
   }
 }
